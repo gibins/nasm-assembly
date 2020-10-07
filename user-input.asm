aa80c99f6012cfa260840912916da643f06bf126
gibin <gibbslabs@gmail.com>
2020-10-07 23:05:35 +0530

    Enter two number and display

--- user-input.asm
+++ user-input.asm
@@ -1,10 +1,16 @@
 section .data
 
-	userMsg db "Please Enter two Numbers"
-	lenUserMsg equ $-userMsg
+	userMsg1 db "Please Enter First Number: "
+	lenUserMsg1 equ $-userMsg1
 
-	dispMsg db "You have entered numbers"
-	lenDispMsg equ $-dispMsg
+	userMsg2 db "Please Enter Second Number: "
+	lenUserMsg2 equ $-userMsg2
+
+	dispMsg1 db "Your first number is "
+	lenDispMsg1 equ $-dispMsg1
+
+	dispMsg2 db "Your second number is "
+	lenDispMsg2 equ $-dispMsg2
 
 section .bss
 
@@ -18,8 +24,8 @@ section .text
 _start:
 	mov eax,4
 	mov ebx,1
-	mov ecx,userMsg
-	mov edx,lenUserMsg
+	mov ecx,userMsg1
+	mov edx,lenUserMsg1
 	int 80h; show first message
 
 	mov eax,3
@@ -27,6 +33,12 @@ _start:
 	mov ecx,num1
 	mov edx,5
 	int 80h; read and store first number
+
+	mov eax,4
+	mov ebx,1
+	mov ecx,userMsg2
+	mov edx,lenUserMsg2
+	int 80h; show second message
 
 	mov eax,3
 	mov ebx,2
@@ -36,8 +48,8 @@ _start:
 
 	mov eax,4
 	mov ebx,1
-	mov ecx,dispMsg
-	mov edx, lenDispMsg
+	mov ecx,dispMsg1
+	mov edx, lenDispMsg1
 	int 80h; display user prompt
 
 	mov eax,4
@@ -48,9 +60,15 @@ _start:
 
 	mov eax,4
 	mov ebx,1
+	mov ecx,dispMsg2
+	mov edx, lenDispMsg2
+	int 80h; display user prompt
+
+	mov eax,4
+	mov ebx,1
 	mov ecx,num2
 	mov edx,5
-	int 80h; display first enterd number
+	int 80h; display second enterd number
 
 	mov eax,1
 	mov ebx,0
